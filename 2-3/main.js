e7eed98cc308be2e55c4eabb73d818614933ac66
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-21 21:01:18 +0900

    Update main.js

--- 2-3/main.js
+++ 2-3/main.js
@@ -1,8 +1,9 @@
 const arg1 = process.argv[2];
 
-if (arg1 <= 23) {
-  console.log (arg1);
-}else {
-  console.log (arg1 % 24);
-}
+// if (arg1 <= 23) {
+//   console.log (arg1);
+// }else {
+// }
+
+console.log (arg1 % 24);
 
