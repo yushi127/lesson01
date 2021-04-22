8419e95704f10fba0829e6eeebcfc375d2fc4671
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-22 23:21:34 +0900

    Update main.js

---
+++ 3-4/main.js
@@ -0,0 +1,21 @@
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+var array1 = [];
+var array2 = [];
+
+let arraySplit1 = arg1.split('');
+for (let i = 0; i < arraySplit1.length; i++) {
+  array1.unshift(arraySplit1[i])
+}
+const bind1 = array1.join('');
+
+let arraySplit2 = arg2.split('');
+for (let j = 0; j < arraySplit2.length; j++) {
+  array2.unshift(arraySplit2[j])
+}
+const bind2 = array2.join('');
+
+const num1 = Number( bind1 );
+const num2 = Number( bind2 );
+
+console.log(num1 + num2);
