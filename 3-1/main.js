bf0e0ff9bc29b0b16623c2b7ae1c6c538b1478e7
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-22 18:30:46 +0900

    Update main.js

--- 3-1/main.js
+++ 3-1/main.js
@@ -3,9 +3,10 @@
     const numb = i;
     for (let i = 1; i <= 9; i++) {
       const box = (numb * i);
-      array.push(box);
+      array.push(`${box} `);
     }
-    console.log(array);
+    var result = array.join('');
+    console.log(result);
     array.length = 0;
   }
 
