b58ffec826e818dad69e82cf4a3fcd3078139de0
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-22 22:33:53 +0900

    Update main.js

--- 3-3/main.js
+++ 3-3/main.js
@@ -1,7 +1,19 @@
 const arg = process.argv[2];
-for (let i = 0; i <= arg; i++) {
-  var array();
-  for (let j = 0; j < i; j++) {
-        
+const div = arg/2;
+var num = Math.floor(div);
+for (let i = 0; i < arg ; i++) {
+  var array = ['*'];
+  for (let y = 1; y < arg; y++) {
+    array.unshift(' ');
+    }
+
+  for (let j = 0; j <= i; j++) {
+    if (j !== 0) {
+      array.push('*','*');
+      array.shift(' ');
+    }    
   }
+
+  var result = array.join('');
+  console.log(result);
 }
