34605b3a85bb8a0d09cfa4b6de890caef526ea88
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-27 17:35:47 +0900

    Update main.js

--- 4-5/main.js
+++ 4-5/main.js
@@ -1,24 +1,14 @@
 var array = [];
 var sub = [];
-for (let i = 2; i < 15; i++) {
-  const arg1 = process.argv[2];
-  const arg2 = process.argv[3];
-  const arg3 = process.argv[4];
-  const arg4 = process.argv[5];
-  const arg5 = process.argv[6];
-  const arg6 = process.argv[7];
-  const arg7 = process.argv[8];
-  const arg8 = process.argv[9];
-  const arg9 = process.argv[10];
-  const arg10 = process.argv[11];
-  const arg11 = process.argv[12];
-  const arg12 = process.argv[13];
-  const arg13 = process.argv[14];
-  array.push (process.argv[i])
-}
 
-for (let j = 0; j <= array.length; j++) {
-  for (let z = 0; z <= array.length; z++) {
+ for (let i = 2; i < process.argv.length; i++) {
+    array.push(Number(process.argv[i])); 
+    }
+
+console.log(array);
+
+for (let j = 0; j <= array.length-1; j++) {
+  for (let z = j; z <= array.length; z++) {
     if (array[j] >= array[z]) {
       sub = array[j];
       console.log('--------'+j+':::::'+z+'---------')
@@ -57,3 +47,23 @@ console.log(array);
 
 
 
+// var array = [];
+// var sub = [];
+// for (let i = 2; i < process.argv.length; i++) {
+//   array.push(Number(process.argv[i])); 
+//   }
+
+
+
+// for (let j = 0; j <= array.length; j++) {
+//   for (let z = 0; z <= array.length; z++) {
+//     if (array[j] >= array[z]) {
+//       sub = array[j];
+//       console.log('--------'+array[j]+':::::'+array[z]+'---------')
+//       array[j] = array[z];
+//       array[z] = sub;
+//       console.log(array);
+//     }
+//   }
+// }
+// console.log(array);
