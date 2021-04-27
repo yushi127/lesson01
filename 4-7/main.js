e6bec09db537bea0fa8fbd23702943037372d8d8
yushi127 <80163799+yushi127@users.noreply.github.com>
2021-04-27 18:28:27 +0900

    Update main.js

---
+++ 4-7/main.js
@@ -0,0 +1,19 @@
+var array = [];
+var sub = [];
+var Newarray = [];
+
+
+for (let i = 2; i < process.argv.length; i++) {
+  array.push(process.argv[i]); 
+}
+
+const sprit = array[0].split( ',' );
+
+for (let i = 0; i < sprit.length; i++) {
+    const inv = sprit[i].toLowerCase().indexOf(array[1])
+    if (inv !== -1) {
+      Newarray.push(sprit[i]);
+    }
+  }
+  const result = Newarray.join();
+  console.log(result);
